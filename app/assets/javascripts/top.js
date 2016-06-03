e58e986048f8bfa94a6e2d59aafc05a24f15a712
Kaito Kinjo <kaitokinjo@gmail.com>
2016-06-04 03:02:05 +0900

    add superagent

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,8 +1,15 @@
+//= require superagent/superagent
+
 window.onload = function () {
   new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue.js!'
     }
-    });
+  });
+  superagent
+  .get('./user')
+  .end(function(err, res){
+    console.log(res.body);
+  });
 }
